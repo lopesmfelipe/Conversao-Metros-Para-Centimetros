4cf37fa59bd69de585204c1fd14dcfe64288f9e0
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-12 15:12:28 -0400

    código definitivo

--- Conversao Metros para Centimetros/src/com/persolnal/classes/ConversaoMetrosParaCentimetros.java
+++ Conversao Metros para Centimetros/src/com/persolnal/classes/ConversaoMetrosParaCentimetros.java
@@ -1,10 +1,19 @@
 package com.persolnal.classes;
 
+import java.util.Scanner;
+
 public class ConversaoMetrosParaCentimetros {
 
 	public static void main(String[] args) {
-			
 		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe uma quantidade de metros");
+		
+		double metros = scan.nextDouble();
+		double centimetros = metros * 100;
+		
+		System.out.println(metros + " metros tem " + centimetros + " centímetros");
+
 		
 		
 
